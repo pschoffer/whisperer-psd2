193bd24f0d85a35e63bc5336fa39de1527939f54
Ercument Kisa <ercument.kisa@klarna.com>
2019-06-08 11:14:23 +0300

    Initial commit

---
+++ src/main/java/com/techsylvania/klarnauts/whisperer/WhispererApplication.java
@@ -0,0 +1,13 @@
+package com.techsylvania.klarnauts.whisperer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhispererApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WhispererApplication.class, args);
+  }
+
+}

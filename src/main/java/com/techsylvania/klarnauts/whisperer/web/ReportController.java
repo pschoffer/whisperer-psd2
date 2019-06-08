97c55a6300b7e95695bc3b3ea49e16a0686234db
Pavel Schoffer <pavel.schoffer@klarna.com>
2019-06-08 16:38:34 +0200

    Report flow

--- src/main/java/com/techsylvania/klarnauts/whisperer/web/ReportController.java
+++ src/main/java/com/techsylvania/klarnauts/whisperer/web/ReportController.java
@@ -3,32 +3,33 @@ package com.techsylvania.klarnauts.whisperer.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techsylvania.klarnauts.whisperer.data.domain.ReportItem;
-import com.techsylvania.klarnauts.whisperer.service.UserService;
+import com.techsylvania.klarnauts.whisperer.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/report")
 public class ReportController {
 
-  private ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
+  private final TransactionService transactionService;
 
   @Autowired
-  public ReportController(ObjectMapper objectMapper) {
+  public ReportController(ObjectMapper objectMapper, final TransactionService transactionService) {
     this.objectMapper = objectMapper;
+    this.transactionService = transactionService;
   }
 
 
-  @GetMapping("/{reportType}")
-  public String getReport(@PathVariable String reportType) throws JsonProcessingException {
-    ReportItem[] items = {new ReportItem("one", 12, 2600),
-        new ReportItem("two", 78, 12600)};
-    return objectMapper.writeValueAsString(items);
+  @GetMapping("/categories")
+  public String getCategoriesReport() throws JsonProcessingException {
+
+    final List<ReportItem> result = transactionService.getAmountsPerCategory();
+    return objectMapper.writeValueAsString(result.toArray());
   }
 }

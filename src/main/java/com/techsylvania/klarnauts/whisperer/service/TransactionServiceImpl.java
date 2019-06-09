560183ee01d3191fa2d380ac424b4e561f615abe
Ercument Kisa <ercument.kisa@klarna.com>
2019-06-09 08:04:32 +0300

    Refactors platinum to gold

--- src/main/java/com/techsylvania/klarnauts/whisperer/service/TransactionServiceImpl.java
+++ src/main/java/com/techsylvania/klarnauts/whisperer/service/TransactionServiceImpl.java
@@ -23,8 +23,8 @@ import java.util.stream.Collectors;
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
-  private static final BigDecimal PLATINUM_AMOUNT = new BigDecimal("50000");
-  private static final BigDecimal GOLD_AMOUNT = new BigDecimal("25000");
+  private static final BigDecimal GOLD_AMOUNT = new BigDecimal("50000");
+  private static final BigDecimal SILVER_AMOUNT = new BigDecimal("25000");
   private static final BigDecimal BRONZE_AMOUNT = new BigDecimal("10000");
   private TransactionRepository transactionRepository;
 
@@ -62,17 +62,17 @@ public class TransactionServiceImpl implements TransactionService {
 
     // do the segmentation logic
     Map<String, Integer> segmentationMap = new HashMap<>();
-    segmentationMap.put("Platinum", 0);
     segmentationMap.put("Gold", 0);
+    segmentationMap.put("Silver", 0);
     segmentationMap.put("Bronze", 0);
     segmentationMap.put("None", 0);
 
     results.forEach(
         result -> {
-          if (PLATINUM_AMOUNT.compareTo(result.totalAmount) >= 0) {
-            segmentationMap.put("Platinum", segmentationMap.get("Platinum") + 1);
-          } else if (GOLD_AMOUNT.compareTo(result.totalAmount) >= 0) {
+          if (GOLD_AMOUNT.compareTo(result.totalAmount) >= 0) {
             segmentationMap.put("Gold", segmentationMap.get("Gold") + 1);
+          } else if (SILVER_AMOUNT.compareTo(result.totalAmount) >= 0) {
+            segmentationMap.put("Silver", segmentationMap.get("Silver") + 1);
           } else if (BRONZE_AMOUNT.compareTo(result.totalAmount) >= 0) {
             segmentationMap.put("Bronze", segmentationMap.get("Bronze") + 1);
           } else {
